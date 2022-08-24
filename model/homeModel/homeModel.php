d17eb19d6349f01d8652cafd0e806ffb2a9a8447
NewLifeOrNewDeath <crdavidlg16@gmail.com>
2022-08-24 17:09:29 -0500

    actualizacion de usuarios y estandarización de ajax y jwt

--- model/homeModel/homeModel.php
+++ model/homeModel/homeModel.php
@@ -29,15 +29,13 @@ class homeModel{
 		$niveles=array();
 		$usuario=$_SESSION["usuario"];
 		$usuario=json_decode($usuario);
-		$sql = "CALL bl_banca(:username,'0001', '', 'login', 'fsql', 'usu_login', :clave, '', '', '', '', '', '', '', :token, '')";
+		$sql = "CALL bl_banca(:username,:banca,:token, 'list', 'fsql', 'menu_usu', '', '', '', '', '', '', '', '', '', '')";
 		$this->conexion=conexion::getConexion();
 		$statemant=$this->conexion->prepare($sql);
 		$statemant->bindParam(":username",$usuario->user);
-		$statemant->bindParam(":clave",$usuario->clave);
 		$statemant->bindParam(":token",$usuario->token);
+		$statemant->bindParam(":banca",$usuario->banca);
 		if($statemant->execute()){
-			$statemant->fetchAll(PDO::FETCH_ASSOC);
-			$statemant->nextRowset();
 			while($row=$statemant->fetch(PDO::FETCH_ASSOC)){
 				array_push($arboles,new Arbol($row["id"],$row["parentid"],$row["icono"],$row["item"],$row["link"],$row["etiqueta"]));
 			}
